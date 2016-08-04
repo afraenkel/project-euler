a485c93c2b75711667adb2cb613ebd441f0fc683
Aaron Fraenkel <aaron.fraenkel@gmail.com>
2016-08-04 09:30:42 -0700

    added grid in prob28

---
+++ python/prob28.py
@@ -0,0 +1,45 @@
+
+import itertools as it
+
+
+def make_grid(N):
+
+    move_dict = [
+        lambda x,y: (x+1,y),
+        lambda x,y: (x,y-1),
+        lambda x,y: (x-1,y),
+        lambda x,y: (x,y+1)
+        ]
+    
+    loc = (0,0)    
+    D = {loc: 0}
+    t = 1
+
+    step_dists = it.zip_longest(
+        it.count(1, 2),
+        it.count(1, 2),
+        it.count(2, 2),
+        it.count(2, 2)
+    )
+
+    for x in step_dists:
+        if x[0] >= N:
+            break
+
+        for d, f in zip(x, move_dict):
+            for _ in range(d):
+                loc = f(*loc)
+                D[loc] = t
+                t += 1
+
+    return D
+
+
+def print_grid(size=10):
+    D = make_grid(size)
+    for x in it.groupby(
+            sorted(D, key=lambda x: (-1*x[1], x[0])),
+            lambda x:x[1]
+    ):
+        row = '|'.join([ '%3d' % D[y] for y in x[1]])
+        print('%s' %row)
