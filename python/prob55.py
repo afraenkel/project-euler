720086624090891ed0a384f788174d2161dbd9b3
Aaron Fraenkel <aaron.fraenkel@gmail.com>
2016-08-04 21:01:15 -0700

    added lychrels (prob55)

---
+++ python/prob55.py
@@ -0,0 +1,20 @@
+'''Lychrel numbers'''
+
+def is_palindrome(x):
+    return str(x) == str(x)[::-1]
+
+def not_lychrel(x):
+    """returns number of iteration for a number
+    to become a palindrome -- else returns 0"""
+    for k in range(50):
+        x += int(str(x)[::-1])
+        if is_palindrome(x):
+            return k+1
+    return 0
+
+def main():
+    H = (not_lychrel(x) for x in range(1, 10000))
+    return len([x for x in H if x == 0])
+
+if __name__ == '__main__':
+    print(main())
